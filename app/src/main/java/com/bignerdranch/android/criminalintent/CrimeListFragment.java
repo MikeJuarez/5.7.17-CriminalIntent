a5992ddc66764a6e6980c67cf5d0873af7583e10
mjuar005 <mjuar005@fiu.edu>
2017-05-17 21:17:50 -0400

    CrimeListFragment change
    - Added code to refresh list with updated changes

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import static android.R.attr.y;
+
 /**
  * Created by user on 5/8/2017.
  */
@@ -33,13 +35,22 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        updateUI();
+    }
     private void updateUI(){
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
-
+        if (mAdapter == null){
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        }
+        else
+            mAdapter.notifyDataSetChanged();
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
